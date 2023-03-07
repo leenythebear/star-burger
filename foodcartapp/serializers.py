82f48cbc733392ca5fb1078022b490ffd59e29f2
leeny_the_bear <leeny@inbox.ru>
2023-03-07 18:39:21 +0600

    rewrite data validation logic with DRF

---
+++ foodcartapp/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework.serializers import ModelSerializer
+
+from foodcartapp.models import OrderProducts, Order
+
+
+class OrderProductsSerializer(ModelSerializer):
+    class Meta:
+        model = OrderProducts
+        fields = ['id', 'product', 'quantity']
+
+
+class OrderSerializer(ModelSerializer):
+    products = OrderProductsSerializer(many=True, allow_empty=False)  # обратите внимание на many=True
+
+    class Meta:
+        model = Order
+        fields = ['firstname', 'lastname', 'phonenumber', 'address', 'products']
--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,16 +1,12 @@
-from collections import defaultdict
-
 from django.http import JsonResponse
 from django.templatetags.static import static
 
-from phonenumber_field.phonenumber import PhoneNumber
-from phonenumbers import NumberParseException
-
-from rest_framework import permissions, status
+from rest_framework import permissions
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 
 from .models import Product, Order, OrderProducts
+from .serializers import OrderSerializer
 
 
 def banners_list_api(request):
@@ -78,52 +74,15 @@ def is_correct_field(data, field_name, field_type):
 @api_view(['POST'])
 @permission_classes((permissions.AllowAny,))
 def register_order(request):
-    order = request.data
-    errors = defaultdict(set)
-    fields = {
-        'firstname': str,
-        'lastname': str,
-        'phonenumber': str,
-        'address': str,
-        'products': list
-    }
-    for field_name, field_type in fields.items():
-        error = is_correct_field(order, field_name, field_type)
-        if error:
-            errors[field_name].add(error)
-    if 'phonenumber' in order and isinstance(order['phonenumber'], str):
-        try:
-            phonenumber = PhoneNumber.from_string(order['phonenumber'], 'RU')
-            if not phonenumber.is_valid():
-                errors['phonenumber'].add('Введен некорректный номер телефона.')
-        except NumberParseException:
-            errors['phonenumber'].add('Введен некорректный номер телефона.')
+    serializer = OrderSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
 
-    product_ids = Product.objects.all().values_list('id', flat=True)
-    for product in order['products']:
-        error = is_correct_field(product, 'product', int)
-        if error:
-            errors['products'].add(error)
-        else:
-            if product['product'] not in product_ids:
-                errors['products'].add(f"Заказ с несуществующим id продукта."
-                                       f"Недопустимый первичный ключ {product['product']}")
-        error = is_correct_field(product, 'quantity', int)
-        if error:
-            errors['products'].add(error)
-        else:
-            if product['quantity'] < 1:
-                errors['products'].add('Количество продуктов должно быть больше 0')
-
-    if errors:
-        return Response({'errors': errors}, status=status.HTTP_400_BAD_REQUEST)
-
-    saved_order = Order.objects.create(firstname=order['firstname'],
-                                       lastname=order['lastname'],
-                                       phonenumber=order['phonenumber'],
-                                       address=order['address'])
-    for product in order['products']:
+    saved_order = Order.objects.create(firstname=serializer.validated_data['firstname'],
+                                       lastname=serializer.validated_data['lastname'],
+                                       phonenumber=serializer.validated_data['phonenumber'],
+                                       address=serializer.validated_data['address'])
+    for product in serializer.validated_data['products']:
         OrderProducts.objects.create(order=saved_order,
-                                     product=Product.objects.get(pk=product['product']),
+                                     product=product['product'],
                                      quantity=product['quantity'])
     return Response({})
