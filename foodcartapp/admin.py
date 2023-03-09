9037792f88a09642ae538a5a375130d3b1562ed1
leeny_the_bear <leeny@inbox.ru>
2023-03-09 18:36:26 +0600

    make a redirect to the manager page after editing

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 from django.shortcuts import reverse
 from django.templatetags.static import static
 from django.utils.html import format_html
+from django.utils.http import url_has_allowed_host_and_scheme
 
 from .models import Product, Order, OrderProducts
 from .models import ProductCategory
@@ -121,3 +123,11 @@ class OrderProductsInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['firstname', 'lastname', 'pk', 'phonenumber', 'address']
     inlines = [OrderProductsInline]
+
+    def response_post_save_change(self, request, obj):
+        res = super().response_post_save_change(request, obj)
+        if "next" in request.GET and url_has_allowed_host_and_scheme(request.GET['next'], allowed_hosts=None):
+            return HttpResponseRedirect(request.GET['next'])
+        else:
+            return res
+
