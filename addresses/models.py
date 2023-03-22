ff90e79230d72993276ee4af8c5705b0abe0ac93
leeny_the_bear <leeny@inbox.ru>
2023-03-22 22:44:43 +0600

    add blank=True

--- addresses/models.py
+++ addresses/models.py
@@ -5,8 +5,8 @@ class Address(models.Model):
     address = models.CharField(
         max_length=200, verbose_name="Адрес", unique=True
     )
-    lat = models.FloatField("Широта", null=True)
-    lon = models.FloatField("Долгота", null=True)
+    lat = models.FloatField("Широта", null=True, blank=True)
+    lon = models.FloatField("Долгота", null=True, blank=True)
     updated_at = models.DateTimeField("Дата обновления координат")
 
     def __str__(self):
