9d80201cb21e37e1d3d2a2dd6169eedaa51a0f22
leeny_the_bear <leeny@inbox.ru>
2023-07-21 12:14:36 +0600

    change deploy script

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -3,22 +3,11 @@
 set -e
 cd /opt/star-burger
 git pull
-source /root/.pyenv/versions/3.9.0/envs/django_39/bin/activate
-source .env
 
-pip install -r requirements.txt
-npm ci --dev
-./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
-
-python3 manage.py migrate --no-input
-python3 manage.py collectstatic --no-input
-
-systemctl daemon-reload
-systemctl restart django.service
-systemctl reload nginx
+docker compose -f docker-compose-prod.yml up -d
 
 ID_COMMIT=`git --git-dir=/opt/star-burger/.git rev-parse HEAD`
-
+..
 curl --request POST \
      --url https://api.rollbar.com/api/1/deploy \
      --header "X-Rollbar-Access-Token: $ROLLBAR_TOKEN" \
